6df5d3af9b0afe35b6d537e553e28a2a2b1087a5
Kate Kolosovska-Shchudlo <kate.shchudlo@gmail.com>
2022-11-20 00:58:16 +0200

    add hover/focus

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@
 
 const headerBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
+const logo = document.querySelector('.header__logo');
 
 headerBurger.addEventListener('click', function (e) {
   headerBurger.classList.toggle('active');
   headerMenu.classList.toggle('active');
   body.classList.toggle('lock');
+});
+
+logo.addEventListener('click', function () {
+  if (headerBurger.classList.contains('active')) {
+    headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    body.classList.toggle('lock');
+  }
 });
